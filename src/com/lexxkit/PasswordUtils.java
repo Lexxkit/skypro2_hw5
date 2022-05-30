3140371231ce542909ae658dcd8c302b0e9eca5a
Lexxkit <lexxkit@gmail.com>
2022-05-30 19:55:15 +0300

    created two custom exceptions for login and password.

--- src/com/lexxkit/PasswordUtils.java
+++ src/com/lexxkit/PasswordUtils.java
@@ -1,35 +1,37 @@
 package com.lexxkit;
 
+import com.lexxkit.exception.WrongLoginException;
+import com.lexxkit.exception.WrongPasswordException;
+
 public class PasswordUtils {
 
     public static boolean checkLoginAndPassword(String login, String password, String confirmPassword) {
         boolean decision = false;
         try {
             decision = checkLogin(login) && checkPassword(password, confirmPassword);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
+        } catch (WrongLoginException e) {
+            e.printStackTrace();
+        } catch (WrongPasswordException e) {
+            e.printStackTrace();
+        } finally {
+            return decision;
         }
-        return decision;
     }
 
     private static boolean isAlphaNumeric(String s) {
         return s != null && s.matches("^[a-zA-Z0-9_]*$");
     }
 
-    private static boolean checkLogin(String login) {
-        if (login.length() > 20){
-            throw new RuntimeException("WrongLoginException");
-        } else if (isAlphaNumeric(login)) {
-            return true;
-        }
-
-        return false;
+    private static boolean checkLogin(String login) throws WrongLoginException {
+        if (login.length() > 20) {
+            throw new WrongLoginException("Login is greater than 20 symbols!");
+        } else return isAlphaNumeric(login);
     }
 
-    private static boolean checkPassword(String password, String confirmPassword) {
+    private static boolean checkPassword(String password, String confirmPassword) throws WrongPasswordException {
         if (!password.equals(confirmPassword)) {
-            throw new RuntimeException("WrongPasswordException");
-        } else if (isAlphaNumeric(password) || password.length() < 20) {
+            throw new WrongPasswordException("Passwords are different!");
+        } else if (isAlphaNumeric(password) && password.length() < 20) {
             return true;
         }
 
